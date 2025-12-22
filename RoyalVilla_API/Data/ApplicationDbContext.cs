7f7279dff12e03f1ab7472f9d7b3c33b5c006669
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-22 18:07:31 +0200

    Adding ApplicationDbContext

---
+++ RoyalVilla_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoyalVilla_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Define your DbSets here
+
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity relationships and constraints here
+        }
+    }
+}
--- RoyalVilla_API/Program.cs
+++ RoyalVilla_API/Program.cs
@@ -1,12 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
+
 builder.Services.AddControllers();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
