1c7cc40e889310bb9b7f55368343f6d225629b70
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-22 20:41:55 +0200

    Adding Endpoint of GetVillaByID

--- RoyalVilla_API/Controllers/VillaController.cs
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -23,5 +23,30 @@ namespace RoyalVilla_API.Controllers
             return Ok(Villas);
         }
 
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Villa>> GetVillaByID(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                    return NotFound("Villa ID must be greater than 0");
+
+                var villa = await _db.Villas.FirstOrDefaultAsync(v => v.Id == id);
+
+                if (villa == null)
+                    return NotFound($"Villa with ID {id} was not found");
+
+                return Ok(villa);
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error occured while retrieving villa with ID {id} : {ex.Message}");
+            }
+
+        }
     }
 }
