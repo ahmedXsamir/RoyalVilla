5ceec04621585734b8f7f2ab54154a7ee137a990
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-23 18:01:26 +0200

    Create custome API response

---
+++ RoyalVilla_API/Controllers/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace RoyalVilla_API.Controllers
+{
+    public class APIResponse<TData>
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; } 
+        public string Message { get; set; } = string.Empty;
+        public TData? Data { get; set; }
+        public object? Errors { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+    }
+}
