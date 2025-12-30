d3e38a7672eb9bb5202dc367c27ac73b2cc52e82
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-30 06:31:09 +0200

    Add AuthController with user registration endpoint
    Introduced AuthController with a POST /register endpoint that accepts registration data and returns a standardized API response. Current implementation is a placeholder for user registration logic.

---
+++ RoyalVilla_API/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoyalVilla_API.Models.DTOs;
+
+namespace RoyalVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+            
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<APIResponse<UserDTO>>> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            var response = APIResponse<UserDTO>.Ok(null!, "User registered successfully");
+            return Ok(response);
+        }
+    }
+}
