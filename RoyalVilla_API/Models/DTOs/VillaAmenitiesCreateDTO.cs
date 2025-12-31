09a6e354b9418c540c6cf2df5d072bc9d212a72c
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-31 16:45:57 +0200

    Add DTOs for villa amenities (create, update, view)
    Added three DTO classes: VillaAmenitiesCreateDTO, VillaAmenitiesDTO, and VillaAmenitiesUpdateDTO. These classes support creating, updating, and viewing villa amenities, and include validation attributes. All are placed in the RoyalVilla_API.Models.DTOs namespace.

---
+++ RoyalVilla_API/Models/DTOs/VillaAmenitiesCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoyalVilla_API.Models.DTOs
+{
+    public class VillaAmenitiesCreateDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        [Required]
+        public int VillId { get; set; }
+    }
+}
---
+++ RoyalVilla_API/Models/DTOs/VillaAmenitiesUpdateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoyalVilla_API.Models.DTOs
+{
+    public class VillaAmenitiesUpdateDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        [Required]
+        public int VillId { get; set; }
+    }
+}
