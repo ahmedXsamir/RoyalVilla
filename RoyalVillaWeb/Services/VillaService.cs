967c8bcabefb49b69adbc4ef311dc9e2fe34a11f
Ahmed Samir <ahmedsemido14@gmail.com>
2026-01-01 21:48:10 +0200

    Adding IVillaService interface and implement it inside VillaService model

---
+++ RoyalVillaWeb/Services/VillaService.cs
@@ -0,0 +1,39 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private new readonly IHttpClientFactory _httpClient;
+        public VillaService(IHttpClientFactory httpClient) : base(httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<T?> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> DeleteAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAllAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
