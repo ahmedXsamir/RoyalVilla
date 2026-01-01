c6d1d086e51506c9bdc5de0ca8088510ebd6ed85
Ahmed Samir <ahmedsemido14@gmail.com>
2026-01-01 20:39:13 +0200

    Adding service folder contains IBaseServices interface contains the action of sending request and getting the response, and adding new class inside the Services folder to implement this interface

---
+++ RoyalVillaWeb/Services/BaseService.cs
@@ -0,0 +1,29 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+using System.Text.Json;
+
+namespace RoyalVillaWeb.Services
+{
+    public class BaseService : IBaseService
+    {
+        public IHttpClientFactory _httpClient;
+
+        private static readonly JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        public APIResponse<object> ResponseModel { get; set; }
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;   
+            ResponseModel = new APIResponse<object>();
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoyalVillaWeb/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+
+namespace RoyalVillaWeb.Services.IServices
+{
+    public interface IBaseService
+    {
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+        APIResponse<object> ResponseModel { get; set; }
+    }
+}
