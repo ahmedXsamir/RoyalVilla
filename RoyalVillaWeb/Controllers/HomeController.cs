87b1145d6b1221ebb5ec4a62977d898364c9da79
Ahmed Samir <ahmedsemido14@gmail.com>
2026-01-02 14:42:12 +0200

    Add VillService in home controller

--- RoyalVillaWeb/Controllers/HomeController.cs
+++ RoyalVillaWeb/Controllers/HomeController.cs
@@ -1,14 +1,42 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
 using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace RoyalVillaWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public HomeController(IVillaService villaService, IMapper mapper)
         {
-            return View();
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaList = new();
+
+            try
+            {
+                var response = await _villaService.GetAllAsync<APIResponse<IEnumerable<VillaDTO>>>("");
+
+                if (response != null && response.Success && response.Data != null)
+                {
+                    villaList = response.Data.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                TempData["error"] = "Something went wrong while fetching villa data.";
+            }
+
+            return View(villaList);
         }
 
         public IActionResult Privacy()
